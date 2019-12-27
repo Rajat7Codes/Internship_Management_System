e348a7dce27724aba5580c2bd1c38a0efd2b31c5
Rajat Patil <patilrajat@805@gmail.com>
2019-12-27 18:36:26 +0530

    added Internship Duration Service

---
+++ Internship/src/main/java/com/iceico/internship/service/InternshipDurationService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.iceico.internship.service;
+
+import com.iceico.internship.model.InternshipDuration;
+
+/**
+ * @author Rajat
+ *
+ */
+public interface InternshipDurationService {
+	public InternshipDuration addInternshipDuration();
+}
