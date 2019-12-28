810dacd50e7272ec489f5187f0734e2cbe411a68
PujaPokale <puja25gp@gmail.com>
2019-12-28 14:24:16 +0530

    college controller added

--- Internship/src/main/java/com/iceico/internship/controller/CollegeController.java
+++ Internship/src/main/java/com/iceico/internship/controller/CollegeController.java
@@ -3,17 +3,44 @@
  */
 package com.iceico.internship.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.iceico.internship.model.College;
+import com.iceico.internship.service.CollegeService;
+
 /**
  * @author Puja
  *
  */
+
+
+@Controller
 public class CollegeController {
 
-	/**
-	 * 
-	 */
+
+	@Autowired
+	CollegeService collegeService;
+	
+	@RequestMapping("/admin/college/")
+	public String saveCollege(@ModelAttribute("college") @Valid College college,BindingResult result, ModelMap model) {
+		if(result.hasErrors()) {
+			return  "addCollegeInfo";
+		}
+		
+		
+		collegeService.saveCollege(college);
+		return "redirect:/admin/college";
+	}
+	
 	public CollegeController() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 }
