ffa25032eaf2b6daae3c2e6b3a172f8491d1edf5
PujaPokale <puja25gp@gmail.com>
2019-12-28 15:06:52 +0530

    college controller updated

--- Internship/src/main/java/com/iceico/internship/controller/CollegeController.java
+++ Internship/src/main/java/com/iceico/internship/controller/CollegeController.java
@@ -9,9 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.iceico.internship.exceptions.ResourceNotFoundException;
 import com.iceico.internship.model.College;
 import com.iceico.internship.service.CollegeService;
 
@@ -28,19 +32,29 @@ public class CollegeController {
 	@Autowired
 	CollegeService collegeService;
 	
-	@RequestMapping("/admin/college/")
+	@PostMapping("/admin/college/save")
 	public String saveCollege(@ModelAttribute("college") @Valid College college,BindingResult result, ModelMap model) {
 		if(result.hasErrors()) {
 			return  "addCollegeInfo";
 		}
-		
-		
 		collegeService.saveCollege(college);
 		return "redirect:/admin/college";
 	}
 	
+	@GetMapping("/admin/college/delete/{id}")
+	public String deleteCollege(@PathVariable("id") @Valid Long id, ModelMap modelMap) throws ResourceNotFoundException {
+	     collegeService.getCollegeById(id);
+	       collegeService.deleteCollege(id);
+	   	return "redirect:/admin/college";
+		}
+		
+	
+	
+	
 	public CollegeController() {
 	
 	}
+	
 
+	
 }
