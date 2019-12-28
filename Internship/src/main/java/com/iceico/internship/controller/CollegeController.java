915419f00d11f19982221e56ec3b15f555b5f765
PujaPokale <puja25gp@gmail.com>
2019-12-28 16:31:41 +0530

    college controller completed

--- Internship/src/main/java/com/iceico/internship/controller/CollegeController.java
+++ Internship/src/main/java/com/iceico/internship/controller/CollegeController.java
@@ -3,6 +3,8 @@
  */
 package com.iceico.internship.controller;
 
+import java.util.Locale;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import com.iceico.internship.exceptions.ResourceNotFoundException;
 import com.iceico.internship.model.College;
@@ -35,19 +37,37 @@ public class CollegeController {
 	@PostMapping("/admin/college/save")
 	public String saveCollege(@ModelAttribute("college") @Valid College college,BindingResult result, ModelMap model) {
 		if(result.hasErrors()) {
-			return  "addCollegeInfo";
+			return  "CollegeInfo";
 		}
 		collegeService.saveCollege(college);
-		return "redirect:/admin/college";
+		return "redirect:/admin/college/all";
 	}
 	
-	@GetMapping("/admin/college/delete/{id}")
+	
+	
+	@PostMapping("/admin/college/delete/{id}")
 	public String deleteCollege(@PathVariable("id") @Valid Long id, ModelMap modelMap) throws ResourceNotFoundException {
 	     collegeService.getCollegeById(id);
 	       collegeService.deleteCollege(id);
-	   	return "redirect:/admin/college";
+	   	return "redirect:/admin/college/all";
 		}
 		
+	
+	@GetMapping(value="/admin/college/all")
+	 
+	public String getCollegeList(ModelMap modelMap, Locale locale) {
+		modelMap.addAttribute("userList",collegeService.getCollegeList());
+		return "CollegeInfo";
+	}
+	
+	@GetMapping(value = "/admin/college/update/{id}")
+	public String editCollege(@PathVariable("id") @Valid Long id,ModelMap modelMap, Locale locale) throws ResourceNotFoundException {
+       College college=collegeService.getCollegeById(id);
+   	   modelMap.addAttribute("college",college);
+   	   modelMap.addAttribute("edit","EDIT");
+   	   //edit=true
+   	return "CollegeInfo";
+	}
 	
 	
 	
