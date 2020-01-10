22cc7a7731712cad80b4250d7b537a375974e814
PujaPokale <puja25gp@gmail.com>
2020-01-10 17:48:23 +0530

    changes in the feesReturnRepository

--- Internship/src/main/java/com/iceico/internship/repository/FeesReturnRepository.java
+++ Internship/src/main/java/com/iceico/internship/repository/FeesReturnRepository.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.iceico.internship.model.FeesReturn;
 
+/**
+ * @author Puja Pokale
+ * @version 0.1
+ * 
+ * 
+ *
+ */
+
 @Repository
 public interface FeesReturnRepository extends JpaRepository<FeesReturn, Long> {
 
